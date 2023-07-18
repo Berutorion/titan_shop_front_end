e3fe83d1f2f43ad482b026312dcc812d8b875c6f
Berutorion <Berutorion>
2023-07-18 08:53:36 +0000

    Pushed from Locofy Builder

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,264 @@
+<template>
+  <div class="form-title-parent">
+    <div class="form-title">
+      <b class="sign-in">Sign in</b>
+    </div>
+    <div class="email-input">
+      <div class="top-content1">
+        <div class="input-labels5">
+          <div class="label3">Label</div>
+        </div>
+        <div class="input-labels6">
+          <div class="label3">Label</div>
+        </div>
+      </div>
+      <div class="input-field-base1">
+        <img class="left-icon2" alt="" src="/left-icon2.svg" />
+        <img class="processorvisa-icon2" alt="" src="/processorvisa.svg" />
+        <input
+          class="input-labels7"
+          type="email"
+          placeholder="Email Adress"
+          maxlength
+          minlength
+          required
+        />
+        <img class="right-icon2" alt="" src="/right-icon1.svg" />
+      </div>
+      <div class="input-labels8">
+        <div class="label3">Description</div>
+      </div>
+    </div>
+    <div class="email-input">
+      <div class="top-content1">
+        <div class="input-labels5">
+          <div class="label3">Label</div>
+        </div>
+        <div class="input-labels6">
+          <div class="label3">Label</div>
+        </div>
+      </div>
+      <div class="input-field-base2">
+        <img class="left-icon3" alt="" src="/left-icon3.svg" />
+        <img class="processorvisa-icon2" alt="" src="/processorvisa.svg" />
+        <input
+          class="input-labels7"
+          type="password"
+          placeholder="Password"
+          maxlength
+          minlength
+          required
+        />
+        <button class="right-button">
+          <img class="icon" alt="" src="/icon.svg" />
+        </button>
+      </div>
+      <div class="input-labels8">
+        <div class="label3">Description</div>
+      </div>
+    </div>
+    <button class="button">
+      <div class="button-base1">
+        <div class="text2">Login</div>
+      </div>
+    </button>
+  </div>
+</template>
+<script>
+  import { defineComponent, ref } from "vue";
+
+  export default defineComponent({
+    name: "LoginForm",
+  });
+</script>
+<style scoped>
+  .sign-in {
+    flex: 1;
+    position: relative;
+  }
+  .form-title {
+    width: 342px;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: flex-start;
+    text-align: center;
+    font-size: var(--font-size-xl);
+    color: var(--color-gray);
+    font-family: var(--font-roboto);
+  }
+  .label3 {
+    position: relative;
+    line-height: 20px;
+    font-weight: 500;
+  }
+  .input-labels5 {
+    flex: 1;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+  }
+  .input-labels6 {
+    display: none;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    text-align: right;
+  }
+  .top-content1 {
+    align-self: stretch;
+    display: none;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    gap: var(--gap-45xl);
+  }
+  .left-icon2 {
+    position: relative;
+    width: 25.04px;
+    height: 24px;
+  }
+  .processorvisa-icon2 {
+    position: relative;
+    width: 35px;
+    height: 24px;
+    display: none;
+  }
+  .input-labels7 {
+    border: none;
+    font-family: var(--public-sans-text-md-regular);
+    font-size: var(--public-sans-text-md-regular-size);
+    background-color: transparent;
+    flex: 1;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+  }
+  .right-icon2 {
+    position: relative;
+    width: 24px;
+    height: 24px;
+    display: none;
+  }
+  .input-field-base1 {
+    align-self: stretch;
+    border-radius: var(--br-9xs);
+    border: 1.5px solid var(--light-neutrals-400);
+    display: flex;
+    flex-direction: row;
+    padding: var(--padding-3xs) var(--padding-xs);
+    align-items: center;
+    justify-content: flex-start;
+    gap: var(--gap-5xs);
+  }
+  .input-labels8 {
+    align-self: stretch;
+    display: none;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    color: var(--light-type-medium);
+  }
+  .email-input {
+    width: 342px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    gap: var(--gap-9xs);
+  }
+  .left-icon3 {
+    position: relative;
+    width: 24px;
+    height: 24px;
+    overflow: hidden;
+    flex-shrink: 0;
+  }
+  .icon {
+    position: absolute;
+    height: 68.75%;
+    width: 86.57%;
+    top: 15.63%;
+    right: 6.72%;
+    bottom: 15.63%;
+    left: 6.71%;
+    max-width: 100%;
+    overflow: hidden;
+    max-height: 100%;
+  }
+  .right-button {
+    cursor: pointer;
+    border: none;
+    padding: 0;
+    background-color: transparent;
+    position: relative;
+    width: 24px;
+    height: 24px;
+  }
+  .input-field-base2 {
+    align-self: stretch;
+    border-radius: var(--br-9xs);
+    border: 1.5px solid var(--light-neutrals-400);
+    display: flex;
+    flex-direction: row;
+    padding: var(--padding-3xs) var(--padding-xs);
+    align-items: center;
+    justify-content: flex-start;
+    gap: var(--gap-5xs);
+  }
+  .text2 {
+    flex: 1;
+    position: relative;
+    font-size: var(--text-sm-semibold-size);
+    line-height: 20px;
+    font-weight: 600;
+    font-family: var(--text-sm-semibold);
+    color: var(--white);
+    text-align: center;
+  }
+  .button-base1 {
+    flex: 1;
+    border-radius: var(--br-5xs);
+    background-color: var(--color-lightseagreen);
+    box-shadow: var(--shadow-xs);
+    border: 1px solid var(--color-lightseagreen);
+    overflow: hidden;
+    display: flex;
+    flex-direction: row;
+    padding: var(--padding-3xs) var(--padding-base);
+    align-items: center;
+    justify-content: center;
+  }
+  .button {
+    cursor: pointer;
+    border: none;
+    padding: 0;
+    background-color: transparent;
+    border-radius: var(--br-5xs);
+    width: 342px;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: flex-start;
+  }
+  .form-title-parent {
+    border-radius: var(--br-4xl);
+    background-color: var(--white);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25) inset;
+    width: 372px;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    padding: var(--padding-xl) var(--padding-3xs);
+    box-sizing: border-box;
+    align-items: center;
+    justify-content: center;
+    gap: var(--gap-31xl);
+    text-align: left;
+    font-size: var(--text-sm-semibold-size);
+    color: var(--light-type-high);
+    font-family: var(--public-sans-text-md-regular);
+  }
+</style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,108 +1,59 @@
 <template>
   <div class="loginpage">
-    <img class="x-circle-icon" alt="" src="/xcircle.svg" />
-    <div class="rectangle-parent">
-      <div class="frame-child" />
-      <img
-        class="water-drops-blue-background-e8-icon"
-        alt=""
-        src="/waterdropsbluebackgrounde8mjvls-1@2x.png"
-      />
-      <div class="frame-item" />
-      <LoginFormComponent />
-      <div class="wlcome-to-win">Wlcome to Win Store.com</div>
+    <div class="title">
       <div class="vector-parent">
-        <img class="frame-inner" alt="" src="/vector-1.svg" />
+        <img class="frame-child" alt="" src="/vector-1.svg" />
+        <div class="frame-item" />
+        <div class="frame-inner" />
         <div class="ellipse-div" />
-        <div class="frame-child1" />
-        <div class="frame-child2" />
         <div class="storepk">store.pk</div>
       </div>
+      <div class="wlcome-to-win">Wlcome to Win Store.com</div>
     </div>
+    <LoginForm />
   </div>
 </template>
 <script>
   import { defineComponent, ref } from "vue";
-  import LoginFormComponent from "../components/LoginFormComponent.vue";
+  import LoginForm from "../components/LoginForm.vue";
 
   export default defineComponent({
     name: "LoginPage",
-    components: { LoginFormComponent },
+    components: { LoginForm },
   });
 </script>
 <style scoped>
-  .x-circle-icon {
-    position: absolute;
-    top: 10px;
-    left: 1031px;
-    width: 28px;
-    height: 28px;
-    overflow: hidden;
-    display: none;
-  }
   .frame-child {
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    background-color: #c4c4c4;
-    width: 1071px;
-    height: 275.99px;
-  }
-  .water-drops-blue-background-e8-icon {
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    width: 1071px;
-    height: 583.38px;
-    object-fit: cover;
-  }
-  .frame-item {
-    position: absolute;
-    top: 222.48px;
-    left: 0px;
-    background-color: var(--color-white);
-    width: 1071px;
-    height: 390.52px;
-  }
-  .wlcome-to-win {
-    position: absolute;
-    top: 74.19px;
-    left: 330px;
-    display: inline-block;
-    width: 400px;
-    height: 31.53px;
-  }
-  .frame-inner {
     position: absolute;
     top: 8.63px;
     left: -2.5px;
     width: 92.5px;
     height: 26.79px;
   }
-  .ellipse-div {
+  .frame-item {
     position: absolute;
     top: 38.02px;
     left: 26.5px;
     border-radius: 50%;
-    background-color: var(--color-white);
+    background-color: var(--white);
     width: 7px;
     height: 6.49px;
   }
-  .frame-child1 {
+  .frame-inner {
     position: absolute;
     top: 38.02px;
     left: 43.5px;
     border-radius: 50%;
-    background-color: var(--color-white);
+    background-color: var(--white);
     width: 7px;
     height: 6.49px;
   }
-  .frame-child2 {
+  .ellipse-div {
     position: absolute;
     top: 0px;
     left: 59.5px;
     border-radius: 50%;
-    background-color: var(--color-white);
+    background-color: var(--white);
     width: 7px;
     height: 6.49px;
   }
@@ -116,28 +67,46 @@
     height: 21.33px;
   }
   .vector-parent {
-    position: absolute;
-    top: 18.55px;
-    left: 442px;
+    position: relative;
     width: 166.5px;
     height: 44.51px;
-    font-size: 19px;
-    font-family: var(--font-century-gothic);
   }
-  .rectangle-parent {
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    width: 1071px;
-    height: 613px;
+  .wlcome-to-win {
+    position: relative;
+    font-size: 32px;
+    font-family: var(--font-roboto);
+    display: inline-block;
+    width: 400px;
+    height: 31.53px;
+    flex-shrink: 0;
+  }
+  .title {
+    align-self: stretch;
+    height: 120px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 11px;
   }
   .loginpage {
     position: relative;
     width: 100%;
-    height: 613px;
+    height: 1024px;
+    display: flex;
+    flex-direction: column;
+    padding: var(--padding-3xs);
+    box-sizing: border-box;
+    align-items: center;
+    justify-content: center;
+    gap: 83px;
+    background-image: url("/public/loginpage@3x.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: top;
     text-align: left;
-    font-size: 32px;
-    color: var(--color-white);
-    font-family: var(--font-roboto);
+    font-size: 19px;
+    color: var(--white);
+    font-family: var(--font-century-gothic);
   }
 </style>
